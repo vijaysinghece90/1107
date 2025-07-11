2525605e1536c44bdb0017240c8039158992cb2a
vijay <vijaysingh.ece90@gmail.com>
2025-07-12 00:29:51 +0530

    vijay-1-commit

--- modules/azurerm_resource_group/main.tf
+++ modules/azurerm_resource_group/main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "resource_group" {
   name     = var.resource_group_name
   location = var.resource_group_location
 }
+
+resource "azurerm_resource_group" "resource_group1" {
+  name     = var.resource_group_name1
+  location = var.resource_group_location
+}
--- modules/azurerm_resource_group/variables.tf
+++ modules/azurerm_resource_group/variables.tf
@@ -7,3 +7,7 @@ variable "resource_group_location" {
   description = "The location of the resource group."
   type        = string  
 }
+variable "resource_group_name1" {
+  description = "The name of the resource group."
+  type        = string  
+}
