47ae9a7a7f9f9dfbe6f12f5680958a444bece5f3
vijay <vijaysingh.ece90@gmail.com>
2025-07-12 00:32:33 +0530

    vijay-2-commit-conflict

--- modules/azurerm_resource_group/main.tf
+++ modules/azurerm_resource_group/main.tf
@@ -3,7 +3,7 @@ resource "azurerm_resource_group" "resource_group" {
   location = var.resource_group_location
 }
 
-resource "azurerm_resource_group" "resource_group1" {
-  name     = var.resource_group_name1
+resource "azurerm_resource_group" "resource_group2" {
+  name     = var.resource_group_name2
   location = var.resource_group_location
 }
